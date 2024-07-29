4a74d016262c23d577c655f2da7006fce96aed1d
TaejungKim <tae0803@naver.com>
2024-07-29 17:09:27 +0900

    Feat : 상세 페이지 수정 중

--- AndroidStudioProjects/Kustaurant/app/src/main/java/com/example/kustaurant/presentation/ui/detail/EvaluateActivity.kt
+++ AndroidStudioProjects/Kustaurant/app/src/main/java/com/example/kustaurant/presentation/ui/detail/EvaluateActivity.kt
@@ -1,19 +1,46 @@
 package com.example.kustaurant.presentation.ui.detail
 
 import android.os.Bundle
+import android.widget.AdapterView.OnItemClickListener
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.kustaurant.R
 import com.example.kustaurant.databinding.ActivityEvaluateBinding
 
 class EvaluateActivity : AppCompatActivity() {
     lateinit var binding : ActivityEvaluateBinding
+    lateinit var keyWordAdapter : EvaluateKeyWordAdapter
+    var keyWordList : ArrayList<String> = arrayListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEvaluateBinding.inflate(layoutInflater)
 
+        initKeyWord()
+        initRecyclerView()
+
         setContentView(binding.root)
+    }
+
+    private fun initKeyWord() {
+        keyWordList.addAll(
+            arrayListOf(
+                "혼밥", "2~4인", "5인 이상", "단체 회식", "배달", "야식", "친구초대", "데이트", "소개팅")
+        )
+    }
+
+    private fun initRecyclerView() {
+        keyWordAdapter = EvaluateKeyWordAdapter(keyWordList)
+        binding.rvKeyword.adapter = keyWordAdapter
+        binding.rvKeyword.layoutManager = GridLayoutManager(this, 4)
+
+        keyWordAdapter.setItemClickListener(object : EvaluateKeyWordAdapter.OnItemClickListener{
+            override fun onItemClicked() {
+
+            }
+
+        })
     }
 }
--- AndroidStudioProjects/Kustaurant/app/src/main/java/com/example/kustaurant/presentation/ui/detail/EvaluateKeyWordAdapter.kt
+++ AndroidStudioProjects/Kustaurant/app/src/main/java/com/example/kustaurant/presentation/ui/detail/EvaluateKeyWordAdapter.kt
@@ -1,27 +1,56 @@
 package com.example.kustaurant.presentation.ui.detail
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
 import androidx.recyclerview.widget.RecyclerView
+import com.example.kustaurant.R
+import com.example.kustaurant.databinding.ItemDetailMenuBinding
 import com.example.kustaurant.databinding.ItemEvaluateKeywordBinding
 
-class EvaluateKeyWordAdapter(val keyword : ArrayList<String>)  : RecyclerView.Adapter<EvaluateKeyWordAdapter.ViewHolder>(){
-    inner class ViewHolder(val binding: ItemEvaluateKeywordBinding) : RecyclerView.ViewHolder(binding.root){
-        fun bind(item : String){
+class EvaluateKeyWordAdapter(private val keywords: ArrayList<String>) : RecyclerView.Adapter<EvaluateKeyWordAdapter.ViewHolder>() {
+    private val selectedPositions: MutableList<Boolean> = MutableList(keywords.size) { false }
 
+    inner class ViewHolder(val binding: ItemEvaluateKeywordBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            val item = keywords[position]
+            val isSelected = selectedPositions[position]
+
+            binding.tvKeyword.text = item
+            updateUI(isSelected, binding)
+
+            binding.clKeyword.setOnClickListener {
+                selectedPositions[position] = !selectedPositions[position]
+                updateUI(selectedPositions[position], binding)
+            }
+        }
+
+        private fun updateUI(isSelected: Boolean, binding: ItemEvaluateKeywordBinding) {
+            if (isSelected) {
+                binding.clKeyword.setBackgroundColor(binding.root.context.getColor(R.color.tier_2))
+                binding.clKeyword.setBackgroundResource(R.drawable.all_radius_100)
+                binding.tvKeyword.setTextColor(binding.root.context.getColor(R.color.signature_1))
+            } else {
+                binding.clKeyword.setBackgroundResource(R.drawable.all_radius_100)
+                binding.tvKeyword.setTextColor(binding.root.context.getColor(R.color.cement_4))
+            }
         }
     }
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): EvaluateKeyWordAdapter.ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemEvaluateKeywordBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: EvaluateKeyWordAdapter.ViewHolder, position: Int) {
-        holder.bind(keyword[position])
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
     }
 
-    override fun getItemCount(): Int = keyword.size
+    override fun getItemCount(): Int = keywords.size
 
+    fun getSelectedItems(): List<String> {
+        return keywords.filterIndexed { index, _ -> selectedPositions[index] }
+    }
 }
