dbee337007853fa61d48594d4a84534093ce037c
TaejungKim <tae0803@naver.com>
2024-09-01 21:11:32 +0900

    #52 new access token 처리 #48 공지사항 웹뷰 구현 완료

--- AndroidStudioProjects/Kustaurant/app/src/main/java/com/kust/kustaurant/data/di/TokenAuthenticator.kt
+++ AndroidStudioProjects/Kustaurant/app/src/main/java/com/kust/kustaurant/data/di/TokenAuthenticator.kt
@@ -19,8 +19,7 @@ import org.json.JSONObject
 
 class TokenAuthenticator(private val context: Context) : Authenticator {
     override fun authenticate(route: Route?, response: Response): Request? {
-        // 400 또는 403 응답을 만료된 토큰으로 간주
-        if (response.code == 400 || response.code == 403) {
+        if (response.code == 401) {
             val token = getAccessToken(context)  // 기존 토큰 가져오기
             val newToken = refreshToken(token)  // 토큰 갱신 로직
             return if (newToken != null) {
@@ -29,33 +28,29 @@ class TokenAuthenticator(private val context: Context) : Authenticator {
                     .header("Authorization", "Bearer $newToken")
                     .build()
             } else {
-                null  // 갱신 실패 시 null 반환
+                null
             }
         }
         return null
     }
 
     private fun refreshToken(oldToken: String?): String? {
-        val refreshTokenRequest = Request.Builder()
-            .url("http://3.35.154.191:8080/api/v1/new-access-token")
-            .post(RequestBody.create("application/json".toMediaTypeOrNull(), ""))
-            .header("Authorization", oldToken ?: "")
-            .build()
-
-        val client = OkHttpClient()
         try {
+            val refreshTokenRequest = Request.Builder()
+                .url("http://3.35.154.191:8080/api/v1/new-access-token")
+                .post(RequestBody.create("application/json".toMediaTypeOrNull(), "{}"))
+                .header("Authorization", oldToken ?: "")
+                .build()
+
+            val client = OkHttpClient()
             val response = client.newCall(refreshTokenRequest).execute()
-            return if (response.isSuccessful) {
+            if (response.isSuccessful) {
                 val body = response.body?.string()
-                JSONObject(body).getString("accessToken")
-            } else {
-                Log.e("TokenAuthenticator", "Failed to refresh token: ${response.body?.string()}")
-                null
+                return JSONObject(body).getString("accessToken")  // 서버 응답 형식에 맞게 수정
             }
         } catch (e: Exception) {
-            Log.e("TokenAuthenticator", "Error during token refresh", e)
-            return null
+            Log.e("TokenAuthenticator", "Failed to refresh token: ${e.localizedMessage}")
         }
+        return null
     }
-
 }
--- AndroidStudioProjects/Kustaurant/app/src/main/java/com/kust/kustaurant/data/di/XAccessTokenInterceptor.kt
+++ AndroidStudioProjects/Kustaurant/app/src/main/java/com/kust/kustaurant/data/di/XAccessTokenInterceptor.kt
@@ -3,66 +3,24 @@ package com.kust.kustaurant.data.di
 import android.content.Context
 import android.util.Log
 import com.kust.kustaurant.data.getAccessToken
-import com.kust.kustaurant.data.saveAccessToken
 import okhttp3.Interceptor
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.RequestBody
 import okhttp3.Response
-import org.json.JSONObject
 
 class XAccessTokenInterceptor(val context: Context) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        var request = chain.request()
-        var response = chain.proceed(request.newBuilder().addHeader(AUTHORIZATION, getAccessToken(context) ?: "").build())
+        val builder = chain.request().newBuilder()
+        val accessToken = getAccessToken(context)
 
-        if (response.code == 400) {
-            synchronized(this) {
-                // 다시 한번 토큰 만료를 확인하여 동시에 여러 요청이 같은 토큰으로 갱신되는 것을 방지
-                val currentToken = getAccessToken(context)
-                if (currentToken != null && currentToken == request.header(AUTHORIZATION)) {
-                    val newToken = refreshToken(currentToken) // 토큰 갱신 로직을 구현해야 함
-                    if (newToken != null) {
-                        saveAccessToken(context, newToken)
-                        // 새 토큰으로 요청 재구성
-                        request = request.newBuilder()
-                            .header(AUTHORIZATION, "Bearer $newToken")
-                            .build()
-                        response.close() // 기존 응답 닫기
-                        response = chain.proceed(request) // 새 요청 실행
-                    }
-                }
-            }
+        accessToken?.let {
+            builder.addHeader("Authorization", it)
+            Log.d("NetworkInterceptor", "Authorization: $it")
         }
 
-        return response
+        return chain.proceed(builder.build())
     }
 
     companion object {
         const val AUTHORIZATION = "Authorization"
-    }
-
-    private fun refreshToken(oldToken: String?): String? {
-        val refreshTokenRequest = Request.Builder()
-            .url("http://3.35.154.191:8080/api/v1/new-access-token")
-            .post(RequestBody.create("application/json".toMediaTypeOrNull(), ""))
-            .header("Authorization", oldToken ?: "")
-            .build()
-
-        val client = OkHttpClient()
-        try {
-            val response = client.newCall(refreshTokenRequest).execute()
-            return if (response.isSuccessful) {
-                val body = response.body?.string()
-                JSONObject(body).getString("accessToken")
-            } else {
-                Log.e("TokenAuthenticator", "Failed to refresh token: ${response.body?.string()}")
-                null
-            }
-        } catch (e: Exception) {
-            Log.e("TokenAuthenticator", "Error during token refresh", e)
-            return null
-        }
     }
 }
