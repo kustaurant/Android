8c837f202eba0b1ce66e6499ec98e30996762403
TaejungKim <tae0803@naver.com>
2024-09-14 17:25:49 +0900

    fix: 공개테스트 용 배포 수정

--- AndroidStudioProjects/Kustaurant/app/src/main/java/com/kust/kustaurant/presentation/ui/community/CommunityBlankFragment.kt
+++ AndroidStudioProjects/Kustaurant/app/src/main/java/com/kust/kustaurant/presentation/ui/community/CommunityBlankFragment.kt
@@ -1,19 +1,33 @@
 package com.kust.kustaurant.presentation.ui.community
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.kust.kustaurant.R
+import com.kust.kustaurant.databinding.FragmentCommunityBinding
+import com.kust.kustaurant.presentation.ui.search.SearchActivity
 
 class CommunityBlankFragment : Fragment() {
+    lateinit var binding : FragmentCommunityBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        binding = FragmentCommunityBinding.inflate(layoutInflater)
+
+        initSearch()
         return inflater.inflate(R.layout.fragment_community_blank, container, false)
     }
+
+    private fun initSearch() {
+        binding.communityFlIvSearch.setOnClickListener {
+            val intent = Intent(requireContext(), SearchActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
