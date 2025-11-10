eea1951336e07d6ccdf4c0ccfa6d4146e6c1897b
BeomBeom2 <whwjdqja20@naver.com>
2025-11-10 16:52:51 +0900

    [Refactor] 전역 이벤트(503 에러, 로그아웃)를 AppEvents에서 관리하도록 수정

--- app/src/main/java/com/kust/kustaurant/domain/common/notice/ServiceDownNotifier.kt
+++
@@ -1,9 +0,0 @@
-package com.kust.kustaurant.domain.common.notice
-
-import com.kust.kustaurant.domain.model.ServiceDownEvent
-import kotlinx.coroutines.flow.SharedFlow
-
-interface ServiceDownNotifier {
-    val events: SharedFlow<ServiceDownEvent>
-    fun notify503(event: ServiceDownEvent)
-}
